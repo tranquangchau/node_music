f9911ebc8ded7801edd455b88dc8cec7435a2d4f
Trần Quang Châu <tranquangchau155@gmail.com>
2016-06-13 23:10:55 +0700

    add code

---
+++ convert_note.php
@@ -0,0 +1,112 @@
+<?php
+
+class convert_node{
+	var $string_after;
+	//public $string_before;
+	function convert_node($string1){
+		$this->string_after=$string1;		
+	}	
+	//dau hoa ban nhac 0#, 7#,ob,7b
+	function go_convert_tremolo_vn_tre24(){
+		//var_dump($this->string_after);
+		$string_before=$this->string_after;
+		$string_before = preg_replace('/(?<=\s)1(?=\s)/', '<span class="c1">Sol</span>', $string_before);
+		//$string_before = preg_replace('/1(?=\s)/', '<span class="c1">Sol', $string_before);
+		//$string_before = preg_replace('/(?<=\s)1/', '<span class="c1">Sol', $string_before);
+				
+		$string_before = preg_replace('/(?<=\s)2(?=\s)/', '<span class="c2">Rê</span>', $string_before);
+		//$string_before = preg_replace('/2(?=\s)/', '<span class="c2">Rê', $string_before);
+		//$string_before = preg_replace('/(?<=\s)2/', '<span class="c2">Rê', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)3(?=\s)/', '<span class="c3">Đô</span>', $string_before);
+		//$string_before = preg_replace('/3(?=\s)/', '<span class="c3">Đô', $string_before);
+		$string_before = preg_replace('/(?<=\s)3/', '<span class="c3">Đô', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)4(?=\s)/', '<span class="c4">Fa</span>', $string_before);
+		//$string_before = preg_replace('/4(?=\s)/', '<span class="c4">Fa', $string_before);
+		$string_before = preg_replace('/(?<=\s)4/', '<span class="c4">Fa', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)5(?=\s)/', '<span class="c5">Mi</span>', $string_before);
+		//$string_before = preg_replace('/5(?=\s)/', '<span class="c5">Mi', $string_before);
+		$string_before = preg_replace('/(?<=\s)5/', '<span class="c5">Mi', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)6(?=\s)/', '<span class="c6">La</span>', $string_before);
+		//$string_before = preg_replace('/6(?=\s)/', '<span class="c6">La', $string_before);
+		$string_before = preg_replace('/(?<=\s)6/', '<span class="c6">La', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)7(?=\s)/', '<span class="c7">Sol</span>', $string_before);
+		//$string_before = preg_replace('/7(?=\s)/', '<span class="c7">Sol', $string_before);
+		$string_before = preg_replace('/(?<=\s)7/', '<span class="c7">Sol', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)8(?=\s)/', '<span class="c8">Si</span>', $string_before);
+		//$string_before = preg_replace('/8(?=\s)/', '<span class="c8">Si</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)8/', '<span class="c8">Si</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)9(?=\s)/', '<span class="c9">Đồ</span>', $string_before);		
+		//$string_before = preg_replace('/9(?=\s)/', '<span class="c9">Đồ', $string_before);
+		$string_before = preg_replace('/(?<=\s)9/', '<span class="c9">Đồ</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)10(?=\s)/', '<span class="c10">Rê</span>', $string_before);
+		$string_before = preg_replace('/10(?=\s)/', '<span class="c10">Rê</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)10/', '<span class="c10">Rê</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)11(?=\s)/', '<span class="c11">Mi</span>', $string_before);
+		$string_before = preg_replace('/11(?=\s)/', '<span class="c11">Mi</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)11/', '<span class="c11">Mi</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)12(?=\s)/', '<span class="c12">Fa</span>', $string_before);
+		$string_before = preg_replace('/12(?=\s)/', '<span class="c12">Fa</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)12/', '<span class="c12">Fa</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)13(?=\s)/', '<span class="c13">Sol</span>', $string_before);
+		$string_before = preg_replace('/13(?=\s)/', '<span class="c13">Sol</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)13/', '<span class="c13">Sol</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)14(?=\s)/', '<span class="c14">La</span>', $string_before);
+		$string_before = preg_replace('/14(?=\s)/', '<span class="c14">La</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)14/', '<span class="c14">La</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)15(?=\s)/', '<span class="c15">Đố</span>', $string_before);
+		$string_before = preg_replace('/15(?=\s)/', '<span class="c15">Đố</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)15/', '<span class="c15">Đố</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)16(?=\s)/', '<span class="c16">Si</span>', $string_before);	
+		$string_before = preg_replace('/16(?=\s)/', '<span class="c16">Si</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)16/', '<span class="c16">Si</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)17(?=\s)/', '<span class="c17">Mi</span>', $string_before);
+		$string_before = preg_replace('/17(?=\s)/', '<span class="c17">Mi</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)17/', '<span class="c17">Mi</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)18(?=\s)/', '<span class="c18">Fa</span>', $string_before);
+		$string_before = preg_replace('/18(?=\s)/', '<span class="c18">Fa</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)18/', '<span class="c18">Fa</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)19(?=\s)/', '<span class="c19">Sol</span>' , $string_before);
+		$string_before = preg_replace('/19(?=\s)/', '<span class="c19">Sol</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)19/', '<span class="c19">Sol</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)20(?=\s)/', '<span class="c20">Fa</span>', $string_before);
+		$string_before = preg_replace('/20(?=\s)/', '<span class="c20">Fa</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)20/', '<span class="c20">Fa</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)21(?=\s)/', '<span class="c21">Đô</span>', $string_before);
+		$string_before = preg_replace('/21(?=\s)/', '<span class="c21">Đô</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)21/', '<span class="c21">Đô</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)22(?=\s)/', '<span class="c22">La</span>', $string_before);
+		$string_before = preg_replace('/22(?=\s)/', '<span class="c22">La</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)22/', '<span class="c22">La</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)23(?=\s)/', '<span class="c23">Mi</span>', $string_before);
+		$string_before = preg_replace('/23(?=\s)/', '<span class="c23">Mi</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)23/', '<span class="c23">Mi</span>', $string_before);
+		
+		$string_before = preg_replace('/(?<=\s)24(?=\s)/', '<span class="c24">Si</span>', $string_before);
+		$string_before = preg_replace('/24(?=\s)/', '<span class="c24">Si</span>', $string_before);
+		$string_before = preg_replace('/(?<=\s)24/', '<span class="c24">Si</span>', $string_before);
+		
+		return $string_before;		
+	}
+	
+}
---
+++ dongthoigian.php
@@ -0,0 +1,76 @@
+<?php
+include "convert_note.php";
+$string= ' 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 ';
+$node = new convert_node($string);
+echo '<pre>' . htmlentities( $node->go_convert_tremolo_vn_tre24() ) . '</pŕe>';
+
+
+echo '<br>';
+echo '<br>';
+
+$string="355-758-0384";
+$search  = array(0,1,2,3,4,5,6,7,8,9);
+$replace = array('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine');
+echo str_replace($search, $replace, $string);
+
+
+echo '<br>';
+echo '<br>';
+
+
+$string="9 9 9 9";
+$search  = array(' 0 ',' 1 ',' 2 ',' 3 ',' 4 ',' 5 ',' 6 ',' 7 ',' 8 ',' 9 ');
+$replace = array('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine');
+echo str_replace($search, $replace, $string);
+
+echo '<br>';
+echo '<br>';
+$a='
+
+<b>Dòng Thời Gian - Nguyễn Hải Phong</b><br>
+<br><br> 9 9 9
+<br> 9 9 9 10 10 10 <br>
+9 9 9 13 13 13 9 9 6 6 <br>
+5 7 6 6 7 7 <br>
+7 5 5 5 6 6 7 7 /9 9 9 12 12 13 13 <br>
+12 10 10 9 9/12 12 10 10 12 11 <br>
+11 12 13 13 11 12 13 13 14 16 <br>
+13 13 13 13 14 14 <br>
+11 12 13 13 11 12 13 13 14 16 <br>
+13/11 13/11 14 <br>
+<br>
+Chorus:<br>
+13 13 13 9 <br>
+9 9 9 9 9 <br>
+7 11 10 9 10 10 10 10 10 13 <br>
+13 13 13 13 13 14 13 13 12 12 <br>
+11 11 10 10 9 9 8 <br>
+<br>
+13 13 13 9 <br>
+9 9 9 9 9 <br>
+7 11 10 9 10 10 10 10 10 13 <br>
+13 13 13 13 13 14 13 13 12 12 <br>
+11 11 10 10 9 9 8 8 9 9 <br>
+<br>
+<iframe width="420" height="315" src="https://www.youtube.com/embed/MUQPVB-dkj0" frameborder="0" allowfullscreen></iframe>
+<br>
+<div style="text-align: center;">
+<object height="315" width="560"><param name="movie" value="http://www.youtube.com/v/qHVPotIB-kw?version=3&amp;hl=en_US">
+<param name="allowFullScreen" value="true">
+<param name="allowscriptaccess" value="always">
+<embed src="http://www.youtube.com/v/qHVPotIB-kw?version=3&amp;hl=en_US" type="application/x-shockwave-flash" width="560" height="315" allowscriptaccess="always" allowfullscreen="true"></object>
+</div>
+';
+$node = new convert_node($a);
+echo '<pre>' . ( $node->go_convert_tremolo_vn_tre24() ) . '</pŕe>';
+
+?>
+
+<style>
+.c9{
+	color:red;
+}
+.c10{
+	color:green;
+}
+</style>
